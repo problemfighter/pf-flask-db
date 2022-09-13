9ddbcb61e2257ff2a870c4052ccb7a9b694f526c
hmtmcse <hmtm.cse@gmail.com>
2022-09-13 13:42:00 +0600

    working on salary

--- pf_flask_db/helper/query_suggestion.py
+++ pf_flask_db/helper/query_suggestion.py
@@ -3,65 +3,65 @@ from pf_flask_db.helper.pf_db_helper import PFDBHelper
 
 class Query(PFDBHelper):
 
-    def all(self):
+    def all(self) -> "Query":
         pass
 
-    def first(self):
+    def first(self) -> "Query":
         pass
 
-    def first_or_404(self, description=None):
+    def first_or_404(self, description=None) -> "Query":
         pass
 
-    def paginate(self, page=None, per_page=None, error_out=True, max_per_page=None):
+    def paginate(self, page=None, per_page=None, error_out=True, max_per_page=None) -> "Query":
         pass
 
-    def count(self):
+    def count(self) -> "Query":
         pass
 
-    def get(self, ident):
+    def get(self, ident) -> "Query":
         pass
 
-    def get_or_404(self, ident, description=None):
+    def get_or_404(self, ident, description=None) -> "Query":
         pass
 
-    def with_entities(self, *entities):
+    def with_entities(self, *entities) -> "Query":
         pass
 
-    def filter(self, *criterion):
+    def filter(self, *criterion) -> "Query":
         return self
 
-    def filter_by(self, **kwargs):
+    def filter_by(self, **kwargs) -> "Query":
         return self
 
-    def order_by(self, *clauses):
+    def order_by(self, *clauses) -> "Query":
         pass
 
-    def group_by(self, *clauses):
+    def group_by(self, *clauses) -> "Query":
         pass
 
-    def having(self, criterion):
+    def having(self, criterion) -> "Query":
         pass
 
-    def join(self, target, *props, **kwargs):
+    def join(self, target, *props, **kwargs) -> "Query":
         pass
 
-    def limit(self, limit):
+    def limit(self, limit) -> "Query":
         pass
 
-    def offset(self, offset):
+    def offset(self, offset) -> "Query":
         pass
 
-    def from_statement(self, statement):
+    def from_statement(self, statement) -> "Query":
         pass
 
-    def one_or_none(self):
+    def one_or_none(self) -> "Query":
         pass
 
-    def one(self):
+    def one(self) -> "Query":
         pass
 
-    def delete(self, synchronize_session="evaluate"):
+    def delete(self, synchronize_session="evaluate") -> "Query":
         pass
 
-    def update(self, values, synchronize_session="evaluate", update_args=None):
+    def update(self, values, synchronize_session="evaluate", update_args=None) -> "Query":
         pass
